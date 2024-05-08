1a963a901045b59b44ce4152d8593645874d4692
Chase <challenj23@gmail.com>
2024-05-08 15:07:49 -0500

    Initial Commit

---
+++ BookRecommendations/BookRecommendationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookRecommendationsApp.swift
+//  BookRecommendations
+//
+//  Created by Chase Allen on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BookRecommendationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
